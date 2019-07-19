e9623db1171581250031c9d0052b6bfae95fbc1c
bswiens <bswiens@sfu.ca>
2019-07-19 00:20:28 -0700

    fixed expiration time to be on node level within the cache, added unit test of single cache instance, fixed cache

--- LRUCacheService.Tests/LRUCacheUnitTest.cs
+++ LRUCacheService.Tests/LRUCacheUnitTest.cs
@@ -2,57 +2,67 @@ using System;
 using Xunit;
 using LRUCacheService;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace LRUCacheService.Tests
 {
     public class LRUCacheUnitTest
     {
-        public class GEOData
-        {
-            public string _location{get;set;}
-            public GEOData(){}
-        }
-
         [Fact]
         public void LRUTest()
         {
+            int cacheCapacity = 3;
+            int expirationInSeconds = 5;
+            LRUCache<int?> cache = new LRUCache<int?>(cacheCapacity, expirationInSeconds);
+            cache.Add(1, 1);
+            int? testOne = cache.Get(1);
+            Assert.Equal(1, testOne);
 
-            LRUCache<GEOData> cache = new LRUCache<GEOData>(3, 20);
+            cache.Add(2, 2);
+            int? testTwo = cache.Get(2);
+            Assert.Equal(2, testTwo);
 
-            GEOData one = new GEOData(){
-                _location = "one"
-            };
-
-            GEOData two = new GEOData(){
-                _location = "two"
-            };
-
-            GEOData three = new GEOData(){
-                _location = "three"
-            };
-
-            GEOData four = new GEOData(){
-                _location = "four"
-            };
+            cache.Add(3, 3); //list order is now 3 2 1  (LRU at far right)
+            int? testThree = cache.Get(3);
+            Assert.Equal(3, testThree);    
             
-            cache.Add(1, one);
-            GEOData testOne = cache.Get(1);
-            Assert.Equal("one", testOne._location);
+            testOne = cache.Get(1); //list order is now 1 3 2 
+            Assert.Equal(1, testOne);
 
-            cache.Add(2, two);
-            GEOData testTwo = cache.Get(1);
-            Assert.Equal("two", testTwo._location);
+            Assert.Null(cache.Get(4));
 
-            cache.Add(3, three);
-            GEOData testThree = cache.Get(1);
-            Assert.Equal("three", testThree._location);
+            cache.Add(4, 4); //list order is now 4 1 3
+            int? testFour = cache.Get(4);
+            Assert.Equal(4, testFour);
 
-            cache.Add(4, four);
-            GEOData testFour = cache.Get(1);
-            Assert.Equal("four", testFour._location);
+            testOne = cache.Get(1); //list order is now 1 4 3
+            Assert.Equal(1, testOne);
 
-            testOne = cache.Get(1);
-            Assert.Null(testOne);
+            testTwo = cache.Get(2);
+            Assert.Null(testTwo);
+
+            cache.Add(3,3); //list order is now 3 1 4
+            cache.Add(2,2); //list order is now 2 3 1
+
+            Assert.Null(cache.Get(4));
+            testThree = cache.Get(3); //list order is now 3 2 1
+            Assert.Equal(3, testThree);
+        }
+
+        [Fact]
+        public void LRUExpirationTime()
+        {
+            int cacheCapacity = 3;
+            int expirationInSeconds = 5;
+            LRUCache<int?> cache = new LRUCache<int?>(cacheCapacity, expirationInSeconds);
+            cache.Add(1,1);
+            Thread.Sleep(5000);
+            Assert.Null(cache.Get(1)); //expired
+            cache.Add(1,1);
+            Thread.Sleep(3000);
+            cache.Get(1); //updated expiration time
+            Thread.Sleep(3000);
+            Assert.Equal(1, cache.Get(1));//not yet expired
         }
     }
 }
