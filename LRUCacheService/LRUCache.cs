77294805df255bf97dc1a75f0c71cd769833205f
bswiens <bswiens@sfu.ca>
2019-07-19 00:23:37 -0700

    fixed expiration time to be on node level within the cache, added unit test of single cache instance, fixed cache

--- LRUCacheService/LRUCache.cs
+++ LRUCacheService/LRUCache.cs
@@ -1,82 +1,155 @@
 ﻿using System;
-using System.Timers;
+using System.Threading.Tasks;
 using System.Collections.Generic;
 
 namespace LRUCacheService
 {
     public class LRUCache<T>
     {
-        private LinkedList<T> _cacheLRU = new LinkedList<T>();
-        private Dictionary<int, T> _cacheLookup = new Dictionary<int, T>();
-        private Dictionary<T, int> _cacheReverseLookup = new Dictionary<T, int>(); //assumes values are unique
-        private Timer _cacheTimer;
+        public class Node
+        {
+            public int _key {get;set;}
+            public T _data {get;set;}
+            public DateTime _timestamp {get;set;}
+            public Node(T data, int key, DateTime now)
+            {
+                _key = key;
+                _data = data;
+                _timestamp = now;
+            }
+        }
+        private LinkedList<Node> _cacheLRU = new LinkedList<Node>(); //to keep order of nodes
+        private Dictionary<int, Node> _cacheLookup = new Dictionary<int, Node>(); //to find nodes quickly
         private static int _cacheCapacity;
-        private static int _cacheExpirationInMilliseconds;
+        private static int _cacheExpirationInSeconds;
+        //private static List<LRUCache<T>> _cacheInstances = new List<LRUCache<T>>(); //contains list of all instances? But need for diff processes
 
         public LRUCache(int capacity, int cacheExpirationInSeconds)
         {
             _cacheCapacity = capacity;
-            _cacheExpirationInMilliseconds = cacheExpirationInSeconds * 1000;
-            _cacheTimer = new Timer(_cacheExpirationInMilliseconds);
-            _cacheTimer.Elapsed += new ElapsedEventHandler(ClearCache);
-            _cacheTimer.Start();
+            _cacheExpirationInSeconds = cacheExpirationInSeconds;
+            //_cacheInstances.Add(this);
         }
 
-        public LinkedListNode<T> Get(int key)
+        public T Get(int key)
         {
             if(!_cacheLookup.ContainsKey(key))
             {
-                return null;
+                return default(T);
             }
 
             //update LRU
-            LinkedListNode<T> found = _cacheLRU.Find(_cacheLookup[key]);
+            LinkedListNode<Node> found = _cacheLRU.Find(_cacheLookup[key]);
+            DateTime lastUsed = found.Value._timestamp; 
             _cacheLRU.Remove(found);
-            _cacheLRU.AddFirst(found);
 
-            _cacheTimer.Stop();
-            _cacheTimer.Start();
-            return found;
+            //check if expired
+            DateTime now = DateTime.Now;
+            if((now - lastUsed).TotalSeconds > _cacheExpirationInSeconds)
+            {
+                _cacheLookup.Remove(key); //remove it from the dictionary
+                //Task.Run(() => BroadcastRemoveNodeToOtherCaches(key)); //start new thread to broadcast removal of this node to other caches?
+                return default(T);
+            }
+            //Task.Run(() => BroadcastUpdateNodeToOtherCaches(key, now)); //start new thread to broadcast update timestamp and move to front of cache to other caches?
+            found.Value._timestamp = now;
+            _cacheLRU.AddFirst(found);
+            return found.Value._data;
         }
 
         public void Add(int key, T value)
         {
-            if(!_cacheLookup.ContainsKey(key) || !_cacheReverseLookup.ContainsKey(value)) //already in cache
+            DateTime now = DateTime.Now;
+            bool isCacheFull = false;
+            if(_cacheLookup.ContainsKey(key))
             {
                 //update LRU
-                LinkedListNode<T> found = _cacheLRU.Find(_cacheLookup[key]);
+                LinkedListNode<Node> found = _cacheLRU.Find(_cacheLookup[key]);
+                found.Value._timestamp = now;
                 _cacheLRU.Remove(found);
                 _cacheLRU.AddFirst(found);
-
-                _cacheTimer.Stop();
-                _cacheTimer.Start();
+                //Task.Run(() => BroadcastUpdateNodeToOtherCaches(key, now));
                 return;
             }
 
-            if(_cacheLRU.Count == _cacheCapacity) //cache is full 
+            int LRUKey = 0;
+            if(_cacheLRU.Count == _cacheCapacity) //cache is full remove LRU
             {
-                T LRUValue = _cacheLRU.Last.Value;
-                int LRUKey = _cacheReverseLookup[LRUValue]; 
-
-                //remove LRU nodes from all three data structures
-                _cacheLRU.RemoveLast(); //LRU node
-                _cacheLookup.Remove(LRUKey);
-                _cacheReverseLookup.Remove(LRUValue);
+                LRUKey = _cacheLRU.Last.Value._key;
+                _cacheLRU.RemoveLast();
+                _cacheLookup.Remove(LRUKey); 
+                isCacheFull = true;
             }
 
-            //add new node to all three data structures
-            _cacheLRU.AddFirst(value);
-            _cacheLookup.Add(key,value);
-            _cacheReverseLookup.Add(value,key);
+            Node newNode = new Node(value,key,now); 
+            _cacheLRU.AddFirst(newNode);
+            _cacheLookup.Add(key,newNode);
 
-            _cacheTimer.Stop();
-            _cacheTimer.Start();
+            if(isCacheFull)
+            {
+                //Task.Run(() => BroadcastRemoveAndAddNodeToOtherCaches(LRUKey,newNode));
+            } 
+            else
+            {
+                //Task.Run(() => BroadcastAddNodeToOtherCaches(newNode));
+            }
         }
 
-        private void ClearCache(object source, ElapsedEventArgs e)
+        #region Communication
+        private void BroadcastRemoveNodeToOtherCaches(int key)
         {
-            _cacheLRU.Clear();
-            _cacheLookup.Clear();
+            //do something 
         }
+
+        private void BroadcastUpdateNodeToOtherCaches(int key, DateTime timestamp)
+        {
+            //do something
+        }
+
+        private void BroadcastAddNodeToOtherCaches(Node nodeToAdd)
+        {
+            //do something
+        }
+
+        private void BroadcastRemoveAndAddNodeToOtherCaches(int keyToRemove, Node nodeToAdd)
+        {
+            //do something
+        }
+
+        public void ReceiveRemoveNodeFromOtherCaches(int key)
+        {
+            LinkedListNode<Node> found = _cacheLRU.Find(_cacheLookup[key]);
+            if(found != null)
+            {
+                _cacheLRU.Remove(found);
+                _cacheLookup.Remove(key);
+            }
+        }
+
+        public void ReceiveUpdateNodeFromOtherCaches(int key, DateTime timestamp)
+        {
+            LinkedListNode<Node> found = _cacheLRU.Find(_cacheLookup[key]);
+            if(found != null)
+            {
+                found.Value._timestamp = timestamp;
+                _cacheLRU.Remove(found);
+                _cacheLRU.AddFirst(found);
+            }
+        }
+
+        public void ReceiveAddNodeFromOtherCaches(Node nodeToAdd)
+        {
+            _cacheLRU.AddFirst(nodeToAdd);
+            _cacheLookup.Add(nodeToAdd._key,nodeToAdd);
+        }
+
+        public void ReceiveRemoveAndAddNodeFromOtherCaches(int keyToRemove, Node nodeToAdd)
+        {
+            _cacheLRU.RemoveLast(); //may need to search by value instead...incase cache order not synced between instances
+            _cacheLookup.Remove(keyToRemove);
+            _cacheLRU.AddFirst(nodeToAdd);
+            _cacheLookup.Add(nodeToAdd._key,nodeToAdd);
+        }
+        #endregion
     }
 }
